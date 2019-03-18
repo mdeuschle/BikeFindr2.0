ef4f365772a20d5e6ef0a97681c2a56607e10b78
Matthew Deuschle <mdeuschle@gmail.com>
2019-03-17 21:29:01 -0700

    refactor DIVVY model layer

---
+++ BikeFindr/Model/Divvy.swift
@@ -0,0 +1,62 @@
+//
+//  Divvy.swift
+//  BikeFindr
+//
+//  Created by Matt Deuschle on 3/25/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct JSON: Decodable {
+    let stationBeanList: [StationBeanList]
+}
+
+struct StationBeanList: Decodable {
+    let stationName: String?
+    let availableDocks: Int?
+    let latitude: Double?
+    let longitude: Double?
+    let status: String?
+}
+
+struct Divvy {
+    let stationBeanList: StationBeanList
+    let distance: Double
+    var coordinate2D: CLLocationCoordinate2D
+    init?(stationBeanList: StationBeanList?, currentLocation: CLLocation?) {
+        guard let stationBeanList = stationBeanList,
+            let lat = stationBeanList.latitude,
+            let lon = stationBeanList.longitude,
+            let currentLocation = currentLocation else { return nil }
+        self.stationBeanList = stationBeanList
+        let location = CLLocation(latitude: lat, longitude: lon)
+        self.distance = currentLocation.distance(from: location)
+        coordinate2D = CLLocationCoordinate2DMake(lat, lon)
+    }
+}
+
+//class Divvy {
+//
+//    var stationName     = ""
+//    var availableBikes  = 0
+//    var lat             = 0.0
+//    var lon             = 0.0
+//    var status          = ""
+//    var distance        = 0.0
+//    var coordinate2D    = CLLocationCoordinate2D()
+//
+//    func initWithData(data: [String:AnyObject], currentLocation: CLLocation) {
+//
+//        stationName     = data["stAddress1"] as! String
+//        availableBikes  = Int(data["availableBikes"] as! Int)
+//        lat             = Double(data["latitude"] as! Double)
+//        lon             = Double(data["longitude"] as! Double)
+//        status          = data["statusValue"] as! String
+//        distance        = currentLocation.distance(from: CLLocation(latitude: lat, longitude: lon))
+//        coordinate2D    = CLLocationCoordinate2DMake(lat, lon)
+//    }
+//}
+
+
